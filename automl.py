8944bdf93172a5fe3c0ba5bb933cd636f4f080e2
Abhijith1311 <65818243+Abhijith1311@users.noreply.github.com>
2021-12-13 20:26:59 +0530

    Update automl.py

--- automl.py
+++ automl.py
@@ -237,9 +237,14 @@ def main():
      models_train,predictions_train,models_test,predictions_test=LazyClassifierdf(X_train, X_test, Y_train, Y_test)
      st.subheader('2. Table of Model Performance')
      st.write('Training set')
-     st.write(predictions_train)
+     st.write(predictions_train)   
+     
+     st.markdown(filedownload(predictions_train,'New_Normalized.csv'), unsafe_allow_html=True)
+
      st.write('Test set')
      st.write(predictions_test)
+     
+     st.markdown(filedownload(predictions_test,'New_Normalized.csv'), unsafe_allow_html=True)
      
 @st.cache      
 def LazyRegressordf( X_train, X_test, Y_train, Y_test):
